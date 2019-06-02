222d8495f3224a0994e60b9a5c7d04fed98df5cf
bparul7 <46653823+bparul7@users.noreply.github.com>
2019-06-02 19:34:51 +0530

    Add files via upload
    basic code for any competitive platform wherein u can write your code in testcase function. This helps to increase your speed during contest and also u don't have to waste your time in writing the basic code.

---
+++ basic_snippet.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+using db = double;
+using pii = pair < int, int >;
+using pll = pair < ll, ll >;
+#define endl "\n"
+const int N = 2e5 + 10;
+const ll inf = 1e15 + 42;
+
+void testCase() {
+	
+}
+
+int main() {
+	ios_base :: sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+	testCase();
+	return 0;
+}
